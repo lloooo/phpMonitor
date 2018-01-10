54db1286b4c74245e3e86c96ab53d0cc68600619
lloooo <409023726@qq.com>
2018-01-11 01:07:06 +0800

    add simple-html-dom

--- zuanke.php
+++ zuanke.php
@@ -1,6 +1,12 @@
 <?php
+use Sunra\PhpSimple\HtmlDomParser;
+
 require 'vendor/autoload.php';
 $client = new \GuzzleHttp\Client();
-$res = $client->request('GET', 'www.zuanke8.com');
-echo $res->getBody();
-echo '_________done_______';
+$res    = $client->request('GET', 'http://www.zuanke8.com/zuixin.php');
+$body    = $res->getBody();
+$dom     = HtmlDomParser::str_get_html($body);
+$element = $dom->find('赚客大家谈');
+var_dump($element);
+
+//zuanke8阿里云上dns解析有误, 导致无法连接到主机, 需设置hosts: 36.248.216.30    www.zuanke8.com
